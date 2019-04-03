9770bac39d337c399f458cc774358d8502e9f751
Will Meadows <MEDS@outlook.com>
2019-04-02 22:50:54 -0500

    Changed from one-hot to binary classification output. Restructured input. Added training and test error plots.

--- src/MLP.m
+++ src/MLP.m
@@ -7,37 +7,49 @@
 %   Tweak values and probably train longer to reduce training error
 %   Begin evaluating using test set
 %   Stretch goal: plot realtime training and validation results
+%   Variable learning rate or change activation function to leaky relu
 
 close all
 clear variables
-
+rng('shuffle')
 if ~exist('lown')
     load('../data/Databreath.mat');
 end
 
-data = [lown;
-        Normaln;
-        highn];
-% Limiting the data to a 5 second interval for simplicity
-data = data(:, 1280:2560);
-data = real(log(fft(data, 2048, 2)));
+data = [lown';
+        Normaln';
+        highn'];
+h1 = data;
+% Segment the data into 5 second intervals
+data = reshape(data, 1280, []);
+data = real(log10(fft(data, 2048)));
 
 % Data Whitening
-data = data - mean(data, 2);
-data = data ./ std(data, 0, 2);
+% The dataset is already 0 mean
+%data = data - mean(data, 2);
+data = data ./ std(data, 0, 'all');
 
-labels = [ones(size(lown, 1), 1);
-        2*ones(size(Normaln, 1), 1);
-        3*ones(size(highn, 1), 1)];
+labels = [ones(1, size(data, 2)/3), 2*ones(1, size(data, 2)/3), 3*ones(1, size(data, 2)/3)];
     
 
-n = size(data, 1); %  The number of data
-train_ratio = .8; %  80% of data used for training 20% for testing
+n = length(labels); %  The number of data
+train_ratio = .99; %  80% of data used for training 20% for testing
 random_set = randperm(n);
 train_set = random_set(1: floor(train_ratio * n));
 test_set = random_set(floor(train_ratio * n)+1 : end);
-train = data(train_set, :);
-test = data(test_set, :);
+% set1 = 1:75;
+% set2 = 76:150;
+% set3 = 151:225;
+% train_set = set1(1: floor(train_ratio * 75));
+% train_set = [train_set set2(1: floor(train_ratio * 75))];
+% train_set = [train_set set3(1: floor(train_ratio * 75))];
+% 
+% test_set = set1(floor(train_ratio * 75)+1 : end);
+% test_set = [test_set set2(floor(train_ratio * 75)+1 : end)];
+% test_set = [test_set set3(floor(train_ratio * 75)+1 : end)];
+
+train = data(:, train_set);
+test = data(:, test_set);
 train_labels = labels(train_set);
 test_labels = labels(test_set);
 
@@ -46,80 +58,47 @@ n_test = length(test_labels);
 
 
 
-num_iterations = 1000; % Number of batches to be run
-eta = .000001; % Learning rate
-num_hidden = 5; % Number of hidden layers
-num_neurons = 200; % Number of neurons in each hidden layer
+num_iterations = 1250;%2100; % Number of batches to be run 30000
+eta = .0000096; % Learning rate
+num_hidden = 3 ; % Number of hidden layers 3
+num_neurons = 300; % Number of neurons in each hidden layer 300
 
 %  Feedforward
-batch_size = 5;
+batch_size = 20;
 num_classes = 3;
-W_in =  rand(size(data, 2), num_neurons) - .5;
+W_in =  rand(size(data, 1), num_neurons) - .5;
 W_h = rand(num_neurons , num_neurons, num_hidden -1) - .5;
-W_out = rand(num_neurons, num_classes) - .5;
-dW_in = zeros(size(data, 2), num_neurons);
-dW_h = zeros(num_neurons, num_neurons, num_hidden -1);
-dW_out = zeros(num_neurons, num_classes);
-error = zeros(1,num_iterations); % Vector to keep track of error for plotting
+W_out = rand(num_neurons, ceil(log2(num_classes))) - .5;
+
+test_period = 50; % How often should the data be validated
+train_error = zeros(1,num_iterations); % Vector to keep track of error for plotting
+test_error = zeros(1, floor(num_iterations/test_period));
 for i = 1:num_iterations
     selection = randperm(n_train, batch_size); %  select random trials from the training set
-    input = data(selection, :);
-    mse = zeros(1, batch_size);
-    for j = 1:batch_size
-        v_in = input(j,:) * W_in;
-        y_in = tanh(v_in);
-        v_h = zeros(num_hidden -1, num_neurons );
-        y_h = zeros(num_hidden - 1, num_neurons);
-        y = y_in;
-        for k = 1:(num_hidden - 1)
-            v_h(k, :) = y * W_h(:,:,k);
-            y = tanh(v_h(k, :));
-            y_h(k, :) = y;
-        end
-        
-        v_out = y * W_out;
-        y_out = tanh(v_out);
-        
-        classification = y_out;
-        expected = -1 * ones(1, 3);
-        expected(train_labels(selection(j))) = 1;
-        
-        e = expected - classification;
-        mse(j) = mean((e).^2);
-        
-        % Backpropogation
-        
-        
-        %start with the output layer
-        grad = 1 - tanh(v_out).^2;
-        delta = e .* grad;
-        dW_out = dW_out + eta * y_h(end, :).' * delta;
-        
-        % hidden layers
-        W = W_out;
-        y = [y_in;
-            y_h];
-        for k = (num_hidden - 1) : -1 : 1
-            grad = 1 - tanh(v_h(k, :)).^2;
-            delta = grad .* (delta * W.');
-            dW_h(:, :, k) = dW_h(:,:,k) + eta * (y(k,:).' * delta);
-            W = W_h(:,:,k);
-        end
-        
-        % input layer
-        grad = 1 - tanh(v_in).^2;
-        delta = grad .* (delta * W_h(:, :, 1).');
-        dW_in = dW_in + eta * (input(j,:).' * delta);
-        
-        
-    end
+    input = train(:, selection);
+    
+    
+    
+    % Long function that handles MLP forward and backward propogation
+    [y_out, dW_in, dW_h, dW_out, mse] =  MLP_prop(input, W_in, W_h, W_out, train_labels(selection), eta);
+    
     W_in = W_in + dW_in;
     W_h = W_h + dW_h;
     W_out = W_out + dW_out;
     
-    error(i) = mean(mse);
+    train_error(i) = mean(mse);
+    if ~mod(i, test_period)
+        for j = 1:size(test, 2)
+            [y_out, dW_in, dW_h, dW_out, mse] =  MLP_prop(test, W_in, W_h, W_out, test_labels, eta);
+            test_error(i/test_period) = mean(mse);
+        end
+    end
 end
 
 figure;
-plot(error)
+plot(movmean(train_error, 5))
+hold on
 title('Training Error')
+plot(linspace(1, num_iterations, num_iterations/test_period), test_error)
+save('weights.mat', 'W_in', 'W_h','W_out')
+
---
+++ src/MLP_prop.m
@@ -0,0 +1,63 @@
+function [y, dW_in, dW_h, dW_out, mse] = MLP_prop(input, W_in, W_h, W_out, labels, eta)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+dW_in = zeros(size(W_in));
+dW_h = zeros(size(W_h));
+dW_out = zeros(size(W_out));
+batch_size = size(input, 2);
+mse = zeros(1, batch_size);
+
+for j = 1:batch_size    %one batch at a time for classification
+        v_in = input(:,j)' * W_in;
+        y_in = tanh(v_in);
+        %y_in = max(0,v_in);
+        
+        v_h = zeros(size(W_h, 3), size(W_h, 1) );
+        y_h = zeros(size(W_h, 3), size(W_h, 1) );
+        y = y_in;
+        for k = 1:size(W_h, 3)
+            v_h(k, :) = y * W_h(:,:,k);
+            y = tanh(v_h(k, :));
+            %y = max(0,v_h(k, :));
+            
+            y_h(k, :) = y;
+        end
+        
+        v_out = y * W_out;
+        y_out = tanh(v_out);
+        
+        
+        % Backpropogation
+        classification = y_out;  %3 different prediciton probabilities
+        expected = [floor(labels(j)/2), mod(labels(j), 2)];
+        e = expected - classification;
+        mse(j) = mean((e).^2);
+        
+        %start with the output layer
+        grad = 1 - y_out.^2;
+        delta = e .* grad;
+        dW_out = dW_out + eta * y_h(end, :).' * delta;
+        
+        % hidden layers
+        W = W_out;
+        y = [y_in;
+            y_h];
+        for k = (size(W_h, 3)) : -1 : 1
+            grad = 1 - tanh(v_h(k, :)).^2;
+            delta = grad .* (delta * W.');
+            dW_h(:, :, k) = dW_h(:,:,k) + eta * (y(k,:).' * delta);
+            W = W_h(:,:,k);
+        end
+        
+        % input layer
+        grad = 1 - tanh(v_in).^2;
+        delta = grad .* (delta * W_h(:, :, 1).');
+        dW_in = dW_in + eta * (input(:,j) * delta);
+        
+        y = y_out;
+        
+    end
+    
+end
+
